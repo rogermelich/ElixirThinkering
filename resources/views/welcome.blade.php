c07128eca0044f887fc47d061409011b53337cee
rogermelich <rogermelich@gmail.com>
2015-11-16 20:56:16 +0100

    Add id="app"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div id="app"></div>
             </div>
         </div>
     </div>
